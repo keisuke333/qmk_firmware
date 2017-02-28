76506fecbd17c21b19dd44293700a5222fd4d4be
keisuke333 <k.yoshida@sansan.com>
2017-02-28 10:26:25 +0900

    Replace LANG1 with LANG2

--- keyboards/ergodox/keymaps/keisuke333/keymap.c
+++ keyboards/ergodox/keymaps/keisuke333/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | +L2  |      |AltShf| LGui |LANG1 |                                       |LANG2 |  Alt |   [  |   ]  | ~L1  |
+ *   | +L2  |      |  Alt | LGui |LANG2 |                                       |LANG1 |  Alt |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  |      |       |      |      |
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,    KC_NO,
         KC_LCTRL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    ALL_T(KC_NO),
-        MO(MDIA),KC_NO,  LALT(KC_LSFT),  KC_LGUI,KC_LANG1,
+        MO(MDIA),KC_NO,  KC_LALT,KC_LGUI,KC_LANG2,
                                                    ALT_T(KC_APP),KC_NO,
                                                                  KC_NO,
                                                  KC_SPC,KC_BSPC,KC_ESC,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LALT(LCTL(KC_DEL)),KC_Y,    KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
                            KC_H,    KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
         KC_NO,             KC_N,    KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
-                           KC_LANG2,KC_LALT,KC_LBRC,KC_RBRC,KC_FN1,
+                           KC_LANG1,KC_RALT,KC_LBRC,KC_RBRC,KC_FN1,
         KC_NO, KC_NO,
         KC_NO,
         KC_ESC,KC_TAB, KC_ENT
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
-                                  KC_TRNS, KC_WBAK, KC_TRNS,
+                                  KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
