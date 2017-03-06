d60dfa5e3ef3f5c7ac8c352b5dd780b4f5805943
keisuke333 <k.yoshida@sansan.com>
2017-03-06 18:32:31 +0900

    Update []の位置を調整

--- keyboards/ergodox/keymaps/keisuke333/keymap.c
+++ keyboards/ergodox/keymaps/keisuke333/keymap.c
@@ -23,10 +23,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |CTLALT|           |CTLALT|   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|  DEL |           |  DEL |------+------+------+------+------+--------|
  * | LCTRL  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------| Hyper|           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | +L2  |      |  Alt | LGui |LANG2 |                                       |LANG1 |  Alt |   [  |   ]  | ~L1  |
+ *   | +L2  | Alt  | Alt  | LGui |LANG2 |                                       |LANG1 | Alt  |      |      | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  |      |       |      |      |
@@ -43,17 +43,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,    KC_NO,
         KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,    LALT(LCTL(KC_DEL)),
         KC_LCTRL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    ALL_T(KC_NO),
-        MO(MDIA),KC_NO,  KC_LALT,KC_LGUI,KC_LANG2,
+        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    KC_LBRC,
+        MO(MDIA),KC_LALT,KC_LALT,KC_LGUI,KC_LANG2,
                                                    ALT_T(KC_APP),KC_NO,
                                                                  KC_NO,
                                                  KC_SPC,KC_BSPC,KC_ESC,
         // right hand
-        KC_NO,             KC_6,    KC_7,   KC_8,   KC_9,   KC_0,    KC_MINS,
-        LALT(LCTL(KC_DEL)),KC_Y,    KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
+        KC_NO,             KC_6,    KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
+        LALT(LCTL(KC_DEL)),KC_Y,    KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,
                            KC_H,    KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-        KC_NO,             KC_N,    KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
-                           KC_LANG1,KC_RALT,KC_LBRC,KC_RBRC,KC_FN1,
+        KC_RBRC,           KC_N,    KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
+                           KC_LANG1,KC_RALT,KC_NO,  KC_NO,  KC_FN1,
         KC_NO, KC_NO,
         KC_NO,
         KC_ESC,KC_NO, KC_ENT
@@ -116,8 +116,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |Brwser|      |       |      |      |      |
- *                                 |      |Back  |------|       |------|Lclick|Rclick|
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|Lclick|Rclick|
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
