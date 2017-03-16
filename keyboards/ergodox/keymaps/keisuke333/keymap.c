ca728f130b5d53e7853e5a8c4a2e8b340fe7194c
keisuke333 <k.yoshida@sansan.com>
2017-03-16 14:23:44 +0900

    Update Escを両側に戻し、Delを移動

--- keyboards/ergodox/keymaps/keisuke333/keymap.c
+++ keyboards/ergodox/keymaps/keisuke333/keymap.c
@@ -29,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | +L2  | Alt  | Alt  | LGui |LANG2 |                                       |LANG1 | Alt  |      |      | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
+ *                                        | Del  |      |       |      |      |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |      |       |      |        |      |
  *                                 | Space|Back  |------|       |------|        |Enter |
- *                                 |      |space |Delete|       | Esc  |        |      |
+ *                                 |      |space | Esc  |       | Esc  |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTRL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    KC_LBRC,
         MO(MDIA),KC_LALT,KC_LALT,KC_LGUI,KC_LANG2,
-                                                          KC_NO, KC_NO,
+                                                         KC_DEL, KC_NO,
                                                                  KC_NO,
-                                                 KC_SPC,KC_BSPC,KC_DEL,
+                                                 KC_SPC,KC_BSPC,KC_ESC,
         // right hand
         KC_NO,             KC_6,    KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
         LALT(LCTL(KC_DEL)),KC_Y,    KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,
