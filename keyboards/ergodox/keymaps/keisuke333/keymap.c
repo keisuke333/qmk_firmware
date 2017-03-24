bcc58d6b7b0a38362d4d7683d995d5a6fa5b4ef9
keisuke333 <k.yoshida@sansan.com>
2017-03-24 14:53:48 +0900

    Update BreakとInsertをキーコンビネーションしやすい位置に調整

--- keyboards/ergodox/keymaps/keisuke333/keymap.c
+++ keyboards/ergodox/keymaps/keisuke333/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | +L2  | Alt  | Alt  | LGui |LANG2 |                                       |LANG1 | Alt  |      |      | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |Delete|Break |       |Insert|      |
+ *                                        |Delete|      |       |Break |Insert|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |      |       |      |        |      |
  *                                 | Space|Back  |------|       |------|        |Enter |
@@ -47,21 +47,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,    KC_NO,
-        KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,    KC_NO,
+        KC_EQL,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,  KC_NO,
+        KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,  KC_NO,
         KC_LCTRL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    KC_LBRC,
+        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_LBRC,
         MO(MDIA),KC_LALT,KC_LALT,KC_LGUI,KC_LANG2,
-                                                        KC_DEL, KC_BRK,
-                                                                 KC_NO,
-                                                 KC_SPC,KC_BSPC,KC_ESC,
+                                                KC_DEL, KC_NO,
+                                                        KC_NO,
+                                        KC_SPC,KC_BSPC,KC_ESC,
         // right hand
         LALT(LCTL(KC_DEL)),KC_6,    KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
         KC_NO             ,KC_Y,    KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,
                            KC_H,    KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
         KC_RBRC,           KC_N,    KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
                            KC_LANG1,KC_RALT,KC_NO,  KC_NO,  KC_FN1,
-        KC_INS, KC_NO,
+        KC_BRK, KC_INS,
         KC_NO,
         KC_ESC, KC_NO, KC_ENT
     ),
