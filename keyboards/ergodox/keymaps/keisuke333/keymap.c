42e4369d581fc402f5ff8a9f04287136a0c52271
keisuke333 <k.yoshida@sansan.com>
2017-05-30 20:38:40 +0900

    Update 未アサインキーをF10, F11, Ctrl+Alt+Breakに

--- keyboards/ergodox/keymaps/keisuke333/keymap.c
+++ keyboards/ergodox/keymaps/keisuke333/keymap.c
@@ -21,10 +21,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |CADel |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |CABrk |           |CADel |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| F10  |           | F11  |------+------+------+------+------+--------|
  * | LCTRL  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,               KC_5, KC_NO,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,               KC_T, KC_NO,
+        KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,               KC_5, LALT(LCTL(KC_BRK)),
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,               KC_T, KC_F10,
         KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,               KC_G,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,               KC_B, KC_NO,
         MO(MDIA), KC_LALT, KC_LALT, KC_LGUI, LT(CURS, KC_LANG2),
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_SPC, KC_BSPC,  KC_ESC,
         // right hand
         LALT(LCTL(KC_DEL)), KC_6,     KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-        KC_NO,              KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
+        KC_F11,             KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
                             KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_NO,              KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                             KC_LANG1, KC_RALT, KC_NO,   KC_NO,  KC_FN1,
