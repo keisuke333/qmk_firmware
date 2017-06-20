22377315cd16ed2f7f58830c0297c6c6a8640a8d
keisuke333 <k.yoshida@sansan.com>
2017-06-20 16:03:36 +0900

    Swap LGui, LAlt

--- keyboards/ergodox/keymaps/keisuke333/keymap.c
+++ keyboards/ergodox/keymaps/keisuke333/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | +L2  | Alt  | Alt  | LGui |LANG2+|                                       |LANG1 | Alt  |      |      | ~L1  |
+ *   | +L2  | Alt  | LGui | Alt  |LANG2+|                                       |LANG1 | Alt  |      |      | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |Delete| Home |       |Break |Insert|
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,               KC_T, KC_F10,
         KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,               KC_G,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,               KC_B, KC_NO,
-        MO(MDIA), KC_LALT, KC_LALT, KC_LGUI, LT(CURS, KC_LANG2),
+        MO(MDIA), KC_LALT, KC_LGUI, KC_LALT, LT(CURS, KC_LANG2),
                                                                KC_DEL, KC_HOME,
                                                                         KC_END,
                                                       KC_SPC, KC_BSPC,  KC_ESC,
