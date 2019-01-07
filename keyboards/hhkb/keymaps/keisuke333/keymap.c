38ae3fa57001ba972669f0472abe74bdbd9e8bdd
yoshida keisuke <k.yoshida@sansan.com>
2019-01-07 10:58:41 +0900

    Update exchange Alt, GUI

--- keyboards/hhkb/keymaps/keisuke333/keymap.c
+++ keyboards/hhkb/keymaps/keisuke333/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------------------+
      * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | Shift     | fn  |
      * +-----------------------------------------------------------------------------------------+
-     *         | LAlt|  LGUI  |               Space               | RGUI  | RAlt|
+     *         | LGUI|  LAlt  |               Space               | RAlt  | RGUI|
      *         `----------------------------------------------------------------´
      */
 
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC,          \
   KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
   KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  MO(HHKB),                    \
-      KC_LALT,  KC_LGUI,               LT(SPFN, KC_SPC),              KC_RGUI,  KC_RALT),
+      KC_LGUI,  KC_LALT,               LT(SPFN, KC_SPC),              KC_RALT,  KC_RGUI),
 
 
 
